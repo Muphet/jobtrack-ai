cf2e36dd97588096db43fe6867bca51e8267d260
Muphet <swizzx@yahoo.pl>
2024-06-10 15:19:21 +0200

    add modules and resolution for string-width package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxthub/core"],
+  modules: [
+    "@nuxthub/core",
+    "@nuxtjs/i18n",
+    "@nuxtjs/color-mode",
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/test-utils/module",
+    "nuxt-icon",
+    "dayjs-nuxt",
+    "nuxt-file-storage",
+    "nuxt-chatgpt"
+  ],
+  chatgpt: {
+    apiKey: "sk-proj-bUFFMuWCCtZZupYtb3EET3BlbkFJTB4fGNmYe8CiiI15b2dm"
+  },
   hub: {
     database: true
+  },
+  fileStorage: {
+    mount: ""
   }
 })
